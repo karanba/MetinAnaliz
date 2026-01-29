9c1ee882e0a488b9aed4d71440574b69975d20aa
Altay Karakus <altay.karakus@piworks.net>
2026-01-29 23:35:22 +0300

    feat: redesign

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { Tag } from 'primeng/tag';
+import { Button } from 'primeng/button';
+import { Dialog } from 'primeng/dialog';
+import { Tooltip } from 'primeng/tooltip';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, Tag],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, Tag, Button, Dialog, Tooltip],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  infoDialogVisible = false;
+
+  showInfoDialog(): void {
+    this.infoDialogVisible = true;
+  }
+}
